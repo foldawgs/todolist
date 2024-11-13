0356cefaebb09e9ca1a32ca475e8f562d0217983
foldawgs <wahyu.isnantia08@gmail.com>
2024-11-13 14:21:12 +0700

    update splash screen

---
+++ lib/design_system/styles/color_collections.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorCollections {
+  static const Color primaryBlue = Color(0xFF043D6A);
+  static const Color primaryRed = Color(0xFF0C12525);
+  static const Color primaryYellow = Color(0xFF0CE9C00);
+  static const Color primaryGreen = Color(0xFF0009954);
+  static const Color colorWhite = Color(0xFFFFFFFF);
+  static const Color colorDeepBlue = Color(0xFF0F2851);
+  static const Color colorLightGrey = Color(0xFFE5E6EE);
+  static const Color colorColdGrey = Color(0xFF415371);
+  static const Color colorDarkGrey = Color(0xFF898D9E);
+  static const Color colorDustGrey = Color(0xFFB0B2C3);
+}
---
+++ lib/design_system/styles/font_collections.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class FontCollections {
+  static const TextStyle h1 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+  );
+
+  static const TextStyle h4 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+
+  static const TextStyle h5 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold,
+    fontSize: 12,
+  );
+
+  static const TextStyle subhead1 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+
+  static const TextStyle subhead2 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+
+  static const TextStyle subhead3 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+  );
+
+  static const TextStyle subhead4 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+  );
+
+  static const TextStyle paragraph1 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+
+  static const TextStyle paragraph2 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  static const TextStyle paragraph3 = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+  );
+}
